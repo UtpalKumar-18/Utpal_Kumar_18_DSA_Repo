a3d6ef291dc72b8c46631dc85656078173d07530
Utpal Kumar <ram1830104@gmail.com>
2026-08-04 12:54:00 +0530

    Time: 7 ms (18.86%), Space: 33.6 MB (28.39%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,3 +1,23 @@
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//         unordered_map<int,int> mp;
+//         for(int i=0;i<nums.size();i++){
+//             mp[nums[i]]++;
+
+//         }
+//         vector<int> ans;
+//         sort(nums.begin(),nums.end());
+//         int first = nums[0];
+//         int last = nums[nums.size()-1];
+//         for(int i=first;i<last;i++){
+//             if(mp.find(i) == mp.end()) ans.push_back(i);
+//         }
+
+//     return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
@@ -6,11 +26,14 @@ public:
             mp[nums[i]]++;
 
         }
+        int maxi=INT_MIN;
+        int mini = INT_MAX;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]>maxi) maxi = nums[i];   
+            if(nums[i]<mini) mini = nums[i]; 
+        }
         vector<int> ans;
-        sort(nums.begin(),nums.end());
-        int first = nums[0];
-        int last = nums[nums.size()-1];
-        for(int i=first;i<last;i++){
+        for(int i=mini;i<maxi;i++){
             if(mp.find(i) == mp.end()) ans.push_back(i);
         }
 
