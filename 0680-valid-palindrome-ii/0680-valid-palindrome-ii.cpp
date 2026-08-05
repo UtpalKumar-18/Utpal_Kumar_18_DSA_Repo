009ac999d470cde71538702f75ef9cfb8eaaa813
Utpal Kumar <ram1830104@gmail.com>
2026-08-05 09:44:04 +0530

    Time: 6 ms (33.95%), Space: 22 MB (77.6%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,55 @@
+// class Solution {
+// public:
+//     bool checkPalindrome(string &s,int i,int j){
+//          while(i<j){
+//             if(s[i]!=s[j]) return false;
+//             i++;
+//             j--;
+//          }
+//          return true;
+//     }
+//     bool validPalindrome(string s) {
+            
+//             bool result = checkPalindrome(s,0,s.size()-1); 
+//             if(result == true) return result;
+//             int n = s.size();
+//             for(int i=0;i<=n-1;i++){
+//             string s1 = s;
+//              s1.erase(s1.begin()+i);
+//             result =   checkPalindrome(s1,0,s1.size()-1);
+//             if(result == true) return result;
+//             }
+//              return false;
+//     }
+
+
+// };
+
+class Solution {
+public:
+    bool checkPalindrome(string &s,int i,int j){
+         while(i<j){
+            if(s[i]!=s[j]) return false;
+            i++;
+            j--;
+         }
+         return true;
+    }
+    bool validPalindrome(string s) {
+        int i = 0;
+        int j = s.size()-1;
+        while(i<j){
+            if(s[i] == s[j]) {
+                i++;
+                j--;
+            }
+            else{
+                return checkPalindrome(s,i+1,j)|| checkPalindrome(s,i,j-1);
+            }
+
+       
+    }
+    return true;
+    }
+
+};
