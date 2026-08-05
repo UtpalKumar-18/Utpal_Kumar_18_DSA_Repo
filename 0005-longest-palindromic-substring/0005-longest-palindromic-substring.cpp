80628580e55408b40ab2d7cfb4bc88231677d00d
Utpal Kumar <ram1830104@gmail.com>
2026-08-05 11:36:04 +0530

    Time: 915 ms (5.89%), Space: 13.8 MB (41.42%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxi = INT_MIN;
+    bool checkPalindrome(string &s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j])
+                 return false;
+            i++;
+            j--;
+         }
+         return true;
+    }
+    string longestPalindrome(string s) {
+            string s1 ="";
+            for(int i=0;i<s.size();i++){
+                for(int j=0;j<s.size();j++){
+                   bool res = checkPalindrome(s,i,j);
+                    if(res == true){
+                        if((j-i)+1>maxi){
+                            maxi = (j-i)+1;
+                            s1 = s.substr(i,(j-i)+1);
+                        }
+                    }
+                }
+            }
+            return s1;
+
+    }
+};
