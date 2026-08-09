d055ad9b1a54a2b76ad16112082732f59c500709
Utpal Kumar <ram1830104@gmail.com>
2026-08-09 17:04:47 +0530

    Time: 0 ms (100%), Space: 17.7 MB (51.29%) - LeetHub

--- 0257-binary-tree-paths/0257-binary-tree-paths.cpp
+++ 0257-binary-tree-paths/0257-binary-tree-paths.cpp
@@ -9,34 +9,64 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     void getpath(TreeNode* root,vector<string> &path,string &eachpath){
+//             if(root == nullptr) return;
+
+//             if(root) eachpath += to_string(root->val);
+//             if(root->left == nullptr && root->right == nullptr){
+//                 path.push_back(eachpath);
+
+//             } 
+//             else{
+//                 eachpath += "->";
+//                 getpath(root->left,path,eachpath);
+//                 getpath(root->right,path,eachpath); 
+//                 eachpath.erase(eachpath.size() - 2);
+
+//             }
+//             string val = to_string(root->val);
+//             eachpath.erase(eachpath.size() - val.size());            
+//     }
+//     vector<string> binaryTreePaths(TreeNode* root) {
+//             vector<string> path;
+//             string eachpath ="";
+//             if(root==nullptr) return path;
+//             getpath(root,path,eachpath);
+//             return path;
+//     }
+// };
+
 class Solution {
 public:
-    void getpath(TreeNode* root,vector<string> &path,string &eachpath){
+    void getpath(TreeNode* root,vector<string> &ans,vector<int> &path){
             if(root == nullptr) return;
+            path.push_back(root->val);
 
-            if(root) eachpath += to_string(root->val);
             if(root->left == nullptr && root->right == nullptr){
-                path.push_back(eachpath);
+                string s = "";
+                for(int x : path){
+                    if(!s.empty()) s+= "->";
+                    s += to_string(x);
 
-            } 
-            else{
-                eachpath += "->";
-                getpath(root->left,path,eachpath);
-                getpath(root->right,path,eachpath); 
-                eachpath.erase(eachpath.size() - 2);
+                }
+                ans.push_back(s);
                 
             }
-            string val = to_string(root->val);
-            eachpath.erase(eachpath.size() - val.size());
+
+            getpath(root->left,ans,path);
+            getpath(root->right,ans,path);
+            path.pop_back();
 
 
-            
+
     }
     vector<string> binaryTreePaths(TreeNode* root) {
-            vector<string> path;
-            string eachpath ="";
-            if(root==nullptr) return path;
-            getpath(root,path,eachpath);
-            return path;
+            vector<string> ans;
+            vector<int> path;
+            if(root==nullptr) return ans;
+            getpath(root,ans,path);
+            return ans;
     }
 };
