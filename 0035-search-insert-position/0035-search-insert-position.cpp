0993a3c349629cd369c31fa28c57095e6dc2149a
Utpal Kumar <ram1830104@gmail.com>
2026-08-20 13:08:24 +0530

    Time: 0 ms (100%), Space: 13.7 MB (43.49%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -1,24 +1,45 @@
+// class Solution {
+// public:
+//     int searchInsert(vector<int>& nums, int target) {
+//         int start=0,end = nums.size()-1,mid=0,index=0;
+//         while (start<=end)
+//         {
+//             mid = end + (start - end)/2;
+//             if (nums[mid]==target)
+//             return mid;
+
+//             else if(nums[mid]<target)
+//             {
+//                 index=mid+1;
+//                 start = mid+1;
+//             }
+//             else
+//             {
+//                 index = mid;
+//                 end = mid-1;
+//             }
+//         }
+//         return index;
+//     }
+// };
+
+
 class Solution {
 public:
     int searchInsert(vector<int>& nums, int target) {
-        int start=0,end = nums.size()-1,mid=0,index=0;
-        while (start<=end)
-        {
-            mid = end + (start - end)/2;
-            if (nums[mid]==target)
-            return mid;
+        int low=0,high = nums.size()-1;
+        int ind = nums.size();
+        while(low<=high){
+            int mid = (low+high)/2;
 
-            else if(nums[mid]<target)
-            {
-                index=mid+1;
-                start = mid+1;
+            if(nums[mid] >= target){
+             ind = mid;
+             high = mid-1;
             }
-            else
-            {
-                index = mid;
-                end = mid-1;
+            else{
+                low = mid +1;
             }
         }
-        return index;
+        return ind;
     }
 };
