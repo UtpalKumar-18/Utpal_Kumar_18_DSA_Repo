9bde357faf3844cd5fd7a0c4cd64d474fdc9598f
Utpal Kumar <ram1830104@gmail.com>
2026-08-27 22:20:03 +0530

    Time: 0 ms (100%), Space: 8.6 MB (14.3%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -7,15 +7,49 @@
 // };
 
 class Solution {
+// public:
+//     int mySqrt(int x) {
+//         long long ans=0;
+//         for(long long i=1;i<=x;i++){
+//             if(i*i<=x) ans = i;
+//             else break;
+//         }
+//         return ans;
+        
+        
+//     }
+// };
+
+// public:
+//     int mySqrt(int x) {
+//         long long ans=0;
+//         for(long long i=1;i<=x;i++){
+//             if(i*i == x) ans =i;
+//             else if(i*i > x){
+//                 ans = i-1;
+//                 break;
+//             }
+//         }
+//         return ans; 
+        
+//     }
+// };
+
 public:
     int mySqrt(int x) {
-        long long ans=0;
-        for(long long i=1;i<=x;i++){
-            if(i*i<=x) ans = i;
-            else break;
+        int low =1,high =x;
+        int ans =1;
+        while(low<=high){
+            long long mid = low + (high-low)/2;
+            long long val = mid*mid;
+            if(val<=x){
+                ans = mid;
+                low = mid+1;
+            }
+            else high = mid-1;
+
+
         }
-        return ans;
-        
-        
+        return high;
     }
 };
