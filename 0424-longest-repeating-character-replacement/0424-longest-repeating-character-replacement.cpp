bae88541952665f25c0d783bf5f206b1b0a924fb
Utpal Kumar <ram1830104@gmail.com>
2026-09-16 15:40:02 +0530

    Time: 3 ms (67.21%), Space: 10.8 MB (93.18%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -21,6 +21,33 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int characterReplacement(string s, int k) {
+//         int n = s.size();
+//         int l =0,r = 0;
+//         int hash[26] = {0};
+//         int max_len = 0,max_freq = 0;
+
+//         while(r < n){
+//             hash[s[r] - 'A']++;
+//             max_freq = max(max_freq,hash[s[r]-'A']);
+            
+//             while((r-l+1) - max_freq > k){
+//                 hash[s[l]-'A']--;
+//                 max_freq =0;
+//                 for(int i =0;i<26;i++){
+//                     max_freq = max(max_freq,hash[i]);
+//                 }
+//                 l++;
+//             }
+//             max_len = max(max_len,r-l+1);
+//             r++;
+//         }
+//         return max_len;
+//     }
+// };
+
 class Solution {
 public:
     int characterReplacement(string s, int k) {
@@ -33,12 +60,8 @@ public:
             hash[s[r] - 'A']++;
             max_freq = max(max_freq,hash[s[r]-'A']);
             
-            while((r-l+1) - max_freq > k){
+            if((r-l+1) - max_freq > k){
                 hash[s[l]-'A']--;
-                max_freq =0;
-                for(int i =0;i<26;i++){
-                    max_freq = max(max_freq,hash[i]);
-                }
                 l++;
             }
             max_len = max(max_len,r-l+1);
