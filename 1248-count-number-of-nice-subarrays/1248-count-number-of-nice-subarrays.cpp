99b96ceaf3adf117a00c0ebaa66e70306091671d
Utpal Kumar <ram1830104@gmail.com>
2026-09-19 19:53:38 +0530

    Time: 12 ms (39.81%), Space: 71.5 MB (47.33%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -19,6 +19,38 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int countNumberofNiceSubarrays(vector<int> &nums, int k){
+//         if(k<0) return 0;
+//          int n = nums.size();
+        
+//        int l=0,r=0;
+//        int cnt_odds = 0;
+//        int cnt_sub =0;
+
+//        while(r<n){
+//             if(nums[r]%2 != 0){
+//                 cnt_odds++;
+//             }
+//             while(cnt_odds > k){
+//                 if(nums[l]%2 != 0) cnt_odds--;
+//                 l++;
+//             }
+//             if(cnt_odds <= k) cnt_sub += r-l+1;
+//             r++;
+//        }
+//        return cnt_sub;
+//     }
+
+//     int numberOfSubarrays(vector<int>& nums, int k) {
+//         int cnt_sub1 = countNumberofNiceSubarrays(nums,k);
+//         int cnt_sub2 = countNumberofNiceSubarrays(nums,k-1);
+//         return cnt_sub1 - cnt_sub2;
+//     }
+// };
+
+
 class Solution {
 public:
     int countNumberofNiceSubarrays(vector<int> &nums, int k){
@@ -26,18 +58,17 @@ public:
          int n = nums.size();
         
        int l=0,r=0;
-       int cnt_odds = 0;
+       int sum = 0;
        int cnt_sub =0;
 
        while(r<n){
-            if(nums[r]%2 != 0){
-                cnt_odds++;
-            }
-            while(cnt_odds > k){
-                if(nums[l]%2 != 0) cnt_odds--;
+
+            sum += nums[r]%2;
+            while(sum > k){
+                sum -= nums[l]%2;
                 l++;
             }
-            if(cnt_odds <= k) cnt_sub += r-l+1;
+            if(sum <= k) cnt_sub += r-l+1;
             r++;
        }
        return cnt_sub;
